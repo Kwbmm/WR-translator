a2680a728d51d4fefc5a699d41fa749a5ca1faba
Marco Ardizzone <kwbmm1990@gmail.com>
2017-02-18 00:43:15 +0100

    Use of python subprocess to substitute the support shell script

--- wordreference.py
+++ wordreference.py
@@ -3,8 +3,12 @@
 from bs4 import BeautifulSoup
 from sys import argv
 from sys import exit
+from sys import exit
 import urllib.request
+import subprocess as sp
 import re
+import argparse
+
 
 def cleanFrom(element):
     try:
@@ -22,10 +26,10 @@ def cleanFrom(element):
 
     wordType = element.em.text
     if wordType != "":
-        fromWrd.append("<b>"+word+"</b> "
-                       +"<small><i>("+wordType+")</i></small>")
+        fromWrd.append("<b>" + word + "</b> " +
+                       "<small><i>(" + wordType + ")</i></small>")
     else:
-        fromWrd.append("<b>"+word+"</b>")
+        fromWrd.append("<b>" + word + "</b>")
 
 
 def cleanTo(element):
@@ -42,27 +46,59 @@ def cleanTo(element):
     wordType = element.em.extract().text
     word = element.text
     if wordType != "":
-        toWrd.append(word+" <small><i>("+wordType+")</i></small>")
+        toWrd.append(word + " <small><i>(" + wordType + ")</i></small>")
     else:
         toWrd.append(word)
 
 
-if len(argv) != 3:
+languages = ["en", "it", "es", "fr"]
+forbidden = ["it", "es", "fr"]
+
+prog_description = "This is used to get a quick translation from italian, french or spanish to english OR viceversa. NOTE: translation from non-english to non-english or from same-language to same-language is forbidden"
+
+parser = argparse.ArgumentParser(description=prog_description)
+
+parser.add_argument("interface_language", help="Interface language", choices=languages)
+parser.add_argument("source_language", help="Source translation language", choices=languages)
+parser.add_argument("target_language", help="Target translation language", choices=languages)
+args = parser.parse_args()
+
+if args.source_language == args.target_language or (args.source_language in forbidden and args.target_language in forbidden):
+    parser.print_help()
     exit(-1)
 
-lang = argv[1]
-searchTerm = argv[2].replace(" ", "+")
+# Interface translaton variables
+translateFrom = dict()
+wordToTranslate = dict()
+translationsFound = dict()
+
+wordToTranslate["en"] = "Word to translate: "
+wordToTranslate["it"] = "Parola da tradurre: "
+wordToTranslate["fr"] = "Mot à traduire: "
+wordToTranslate["es"] = "Palabra para traducir: "
+
+translationsFound["en"] = "Translations: "
+translationsFound["it"] = "Traduzioni: "
+translationsFound["fr"] = "Traductions: "
+translationsFound["es"] = "Traducciones: "
+
+word_to_translate_cmd = "echo ' ' | rofi -dmenu -p '{}'".format(wordToTranslate[args.interface_language])
+# Run the command then remove the ending newline, and decode the bytestring as a normal utf-8 string
+word_to_translate = sp.run(word_to_translate_cmd, shell=True, stdout=sp.PIPE).stdout[:-1].decode('utf-8')
+
+lang = args.source_language + args.target_language
+searchTerm = str(word_to_translate).replace(" ", "+")
 url = "http://www.wordreference.com/"
 
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
 headers = {'User-Agent': user_agent}
 
-req = urllib.request.Request(url+lang+"/"+searchTerm, headers=headers)
+req = urllib.request.Request(url + lang + "/" + searchTerm, headers=headers)
 
 page = urllib.request.urlopen(req).read()
 
 soup = BeautifulSoup(page, 'html.parser')
-selector = "table.WRD > tr[id^='"+lang+"']"
+selector = "table.WRD > tr[id^='" + lang + "']"
 fromWrd, toWrd = [], []
 
 for res in soup.select(selector):
@@ -71,8 +107,14 @@ for res in soup.select(selector):
     for inner in res.select("td.ToWrd"):
         cleanTo(inner)
 
-if len(fromWrd) == 0 or len(toWrd) == 0 :
-    print("<b>NO TRANSLATIONS FOUND</b>")
-else :
+
+output = ""
+if len(fromWrd) == 0 or len(toWrd) == 0:
+    output = "<b>NO TRANSLATIONS FOUND</b>\n"
+else:
     for frm, to in zip(fromWrd, toWrd):
-        print(frm, to, sep=": ")
+        output += "{}: {}\n".format(frm, to)
+# Remove last newline
+output = output[:-1]
+print_translation_cmd = "echo \"{}\" | rofi -markup-rows -i -dmenu -p \"{}\"".format(output, translationsFound[args.interface_language])
+sp.run(print_translation_cmd, shell=True, stdout=sp.PIPE)
